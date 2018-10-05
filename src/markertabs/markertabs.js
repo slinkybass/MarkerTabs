85acec991fe0867fe1db3a79680176fa34ff5ba6
Soporte04 <soporte04@lansoft.es>
2018-10-05 12:12:44 +0200

    Changed API URL

--- src/markertabs/markertabs.js
+++ src/markertabs/markertabs.js
@@ -1,6 +1,6 @@
 var markertabs = angular.module('markertabs', ['ui.router', 'satellizer', 'ui.bootstrap', 'pageslide-directive']);
 
-markertabs.constant('urlBackend', "http://projects.localhost.com/markertabsBackend/web/app_dev.php");
+markertabs.constant('urlBackend', "http://projects.localhost.com/MarkertabsAPI/web/app_dev.php");
 
 markertabs.config(function ($authProvider, urlBackend) {
     $authProvider.loginUrl = urlBackend + "/users/login";
