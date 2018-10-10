9957231be4c5c4b654852a98e6dfbc7b84c75ffa
Soporte04 <soporte04@lansoft.es>
2018-10-10 10:04:07 +0200

    SSL API URL

--- src/constant.js
+++ src/constant.js
@@ -1,7 +1,7 @@
 'use strict';
 angular.module('app')
     .constant('urlBackend',
-        "http://markertabsapi.garaballu.com"
+        "https://markertabsapi.garaballu.com"
     )
     .constant('MODULES', [
     {
